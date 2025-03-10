19478b271fe7ef1a5f01e41ba704c04998aef874
Fabio Teixeira <fabio1079@gmail.com>
2025-03-10 14:21:10 -0300

    call catppuccin as theme

--- config.lua
+++ config.lua
@@ -19,3 +19,6 @@ vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { noremap = true, silent =
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
+
+-- Theme
+vim.cmd.colorscheme "catppuccin"
